cdc895077961caba2e61237460308a68de2d4649
obulareddynew <64014168+obulareddynew@users.noreply.github.com>
2022-12-12 15:43:20 +0530

    Update settings.py
    Just commit

--- SampleDX/settings.py
+++ SampleDX/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-djo4*tsvn!x$)zf&56^z4xqqu_1iwttv&_^8wmfhphu^w9(swb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
