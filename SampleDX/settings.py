fcc38e3c3568e1ad272cbc23ef3a992345233e99
obulareddynew <64014168+obulareddynew@users.noreply.github.com>
2022-12-12 15:49:06 +0530

    Update settings.py
    Just commit

--- SampleDX/settings.py
+++ SampleDX/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-djo4*tsvn!x$)zf&56^z4xqqu_1iwttv&_^8wmfhphu^w9(swb'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
